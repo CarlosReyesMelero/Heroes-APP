68af9be1047e3fd8bfeddfa9826e873a217ab186
CarlosReyesMelero <creyesmelero@gmail.com>
2024-08-14 18:41:33 +0200

    HeroesService implemented

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/hero.interface';
+import { HereosService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styleUrl: './list-page.component.css'
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit{
+
+  public heroes: Hero[] = [];
+
+  constructor (private hereosService: HereosService) {}
+
+  ngOnInit(): void {
+    this.hereosService.getHeroes()
+      .subscribe( heroes => this.heroes = heroes);
+  }
 
 }
