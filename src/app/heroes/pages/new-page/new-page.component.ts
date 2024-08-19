fd4bafff2fd922f3420d4d6ae2fe4762108f5ec9
CarlosReyesMelero <creyesmelero@gmail.com>
2024-08-19 18:08:14 +0200

    Form-Template and Getters

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { __values } from 'tslib';
+import { HereosService } from '../../services/heroes.service';
 
 
 @Component({
@@ -10,13 +12,13 @@ import { Publisher } from '../../interfaces/hero.interface';
 })
 export class NewPageComponent {
 
-  public heroForm = new FormControl ({
-    id:               new FormGroup<string>(''),
+  public heroForm = new FormGroup ({
+    id:               new FormControl<string>(''),
     superhero:        new FormControl<string>('', { nonNullable: true }),
     publisher:        new FormControl<Publisher>( Publisher.DCComics ),
-    alter_ego:        new FormControl<string>(''),
-    first_appearance: new FormControl<string>(''),
-    characters:       new FormControl<string>(''),
+    alter_ego:        new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters:       new FormControl(''),
     alt_img:          new FormControl(''),
   });
 
@@ -24,5 +26,18 @@ export class NewPageComponent {
     {id: 'DC Comics', desc: 'DC-Comics'},
     {id: 'Marvel Comics', desc: 'marvel-Comics'}
   ];
+
+  constructor ( private heroesService: HereosService ) {}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit ():void {
+    if ( this.heroForm.invalid) return;
+
+    // this.heroesService.updateHero ( this.heroForm.value );
+  }
 
 }
--- src/app/heroes/pipes/heroeImage.pipe.ts
+++ src/app/heroes/pipes/heroeImage.pipe.ts
@@ -9,7 +9,7 @@ export class HeroeImagePipe implements PipeTransform {
   transform(hero: Hero): string {
 
     if ( !hero.id && !hero.alt_img) {
-      return 'assets/no-image.jpg';
+      return 'assets/no-image.png';
     }
 
     if ( hero.alt_img ) return hero.alt_img;
